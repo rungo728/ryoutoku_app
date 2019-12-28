e08768a996aadfbe43c7aeac700e49509748aa89
rungo728 <rufii72865@gmail.com>
2019-12-28 19:35:54 +0900

    Update deploy.rb
    自動デプロイの際に記述が間違っていたので、修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/ssh/KA.pem'] 
+                  keys: ['~/.ssh/KA.pem'] 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 # Unicornの設定ファイルの場所
