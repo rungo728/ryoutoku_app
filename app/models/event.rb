8ed418ae6ea26e7c38416bfb39b197d441b52369
rungo728 <rufii72865@gmail.com>
2020-01-17 23:53:07 +0900

    set association
    イベントテーブルのアソシエーションを設定、userテーブルの「id」とeventsテーブルの「buyer_id」「exhibitor_id」が紐づくようにしています。

---
+++ app/models/event.rb
@@ -0,0 +1,8 @@
+class Event < ApplicationRecord
+  belongs_to :exhibitor, class_name: “User”
+  belongs_to :buyer, class_name: “User”
+  belongs_to :prefecture
+  belongs_to :category
+  has_many :comments
+  has_many :images
+end
