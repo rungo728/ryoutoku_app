644cb7b4d24ec7cad6a08ccfecdbd8ed0c82af50
rungo728 <rufii72865@gmail.com>
2020-01-19 20:29:42 +0900

    update price.js
    やってみたい料理工程をチェックすると割引料金が表示され、チェックした分の合計が表示される、またチェックは２つまでしかできないように実装

--- app/assets/javascripts/price_form.js
+++ app/assets/javascripts/price_form.js
@@ -1,9 +1,41 @@
-// レベルをプルダウンで選んで、計算ボタンを押した時に割引料金が表示される処理
-function cooking_waribiki(ele, select_id) {
-  var element = document.getElementById(select_id);
-  var str = element.value;
-  var cooking_id = "cooking_" + select_id;
-  document.getElementById(cooking_id).textContent = str;
+// レベルをチェックボックスで選んで、割引料金欄に合計額が表示される処理
+ck_suu=5; //チェックボックスの数。
+lim =2;   //チェックできる数
+discount=new Array();//チェックの有無を格納する配列
+discount[0]=500;
+discount[1]=1000;
+discount[2]=1500;
+discount[3]=2000;
+discount[4]=2500;
+function keisan(){
+goukei=0;
+for(i=0;i<=4;i++){
+if(document.cooking_details.elements[i].checked==true){
+goukei=goukei+discount[i];
+}
+}
+document.cooking_details.total.value=goukei;
+}
+// チェックボックスを２つ選んだらそれ以上チェックできないようにする処理
+checkbox=5;   //チェックボックスの数。
+lim =2;   //チェックできる数
+Limit = new Array();   //チェックの有無を格納する配列
+function limit(){
+v=0;   //チェックの合計
+for (i=0; i<checkbox; i++){
+Limit[i]=i;
+if (document.cooking_details.elements[i].checked){Limit[i]="chk";　
+v++;}
+}
+if(v>=lim){
+for (i=0; i<checkbox; i++){
+if(Limit[i]=="chk"){document.cooking_details.elements[i].disabled = "";}
+else{document.cooking_details.elements[i].disabled = true;}
+}
+}
+else{for (i=0; i<checkbox; i++)
+document.cooking_details.elements[i].disabled = "";}
+
 }
 // ここがまだ不完全↓
 // 割引額の合計を <div class="event_middle-row__profit">に表示させる
@@ -13,30 +45,6 @@ window.addEventListener('DOMContentLoaded', function(e){
   document.querySelector('span#result').textContent=val_a+val_b;
 });
 $(document).on('turbolinks:load', function(){
-  // ここもまだ不完全↓
-  // 一つめで選んだレベルは２つ目では選べないようにする処理
-  $('select').change(function () {
-    $('select option[value="0"]').show()
-    $('select').each(function (index, sel) {
-      var disableValue = $(sel).val();
-      $('select option[value="0"]:contains(' +disableValue+ ')').hide()
-    })
-  });
-
-  // $(function () {
-  //   var opt=$('select#waribiki2 option[value="0"]').clone();
-  //   $("select#waribiki").on('change',function(){
-  //     var str = $(this).val();
-  //     if(str=="0"){
-  //       $('select#waribiki2 option:first').before(opt);
-  //     }else{
-  //       $('select#waribiki2 option[value="300"]').hide();
-  //     }
-  //   });
-  // });
-
-
-
   // $('#price-setting').ready(function(){
   //   var inputPrice = $('#price-setting').val();
 
