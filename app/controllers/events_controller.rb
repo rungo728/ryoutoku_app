a159ed1fed7aab5f1b6d0f9c6ef29ab8622f0cbf
rungo728 <rufii72865@gmail.com>
2020-01-19 23:31:54 +0900

    small fix
    画面名のみ記述

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,15 +4,18 @@ class EventsController < ApplicationController
   def index
     
   end
-
+  # イベント出展画面
   def new
-
   end
 
+  # イベント詳細画面
   def show
-
+    
+    # @event = Event.find(params[:id])
+    # @user = User.find_by(id: @event.exhibitor_id)
   end
 
+  # イベント予約確認画面
   def confirmation
     
   end
