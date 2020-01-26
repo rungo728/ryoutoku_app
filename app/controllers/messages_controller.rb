6c9e41589ca0223adf78fa4f1d5d6482efb9714c
rungo728 <rufii72865@gmail.com>
2020-01-26 23:47:01 +0900

    create messages html
    イベント予約者トークページの作成（html+controller）

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,14 @@
 class MessagesController < ApplicationController
+  before_action :get_event
 
   def index
     
   end
 
+  
+  private
+
+  def get_event
+    @user = User.find(current_user.id)
+  end
 end
