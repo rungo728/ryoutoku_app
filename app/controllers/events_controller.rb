a2c333dbf16d63d1931b1b14a142ccd0b848c0ad
rungo728 <rufii72865@gmail.com>
2020-01-28 21:21:47 +0900

    resolve error
    ログアウトする際にエラーが起こったので、htmlの記述を変更して解決

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -23,6 +23,10 @@ class EventsController < ApplicationController
   private
 
   def get_event
-    @user = User.find(current_user.id)
+    if user_signed_in?
+      @user = User.find(current_user.id)
+    else
+      @user = User.new
+    end
   end
 end
