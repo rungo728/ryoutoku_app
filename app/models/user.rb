db33490a3c58297ee243f75c53280cbb5946bcd8
rungo728 <rufii72865@gmail.com>
2020-01-31 01:21:29 +0900

    add association
    「exhibitor_id(出展者)はいるが、まだ、buyer_id(予約者)はいないeventのレコード」等、出展中か予約済かで取ってくるレコードを変えるために記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_many :messages
-         has_many :events
-         has_many :comments
-         has_many :cards
-         has_one  :personal
-         accepts_nested_attributes_for :personal
-         belongs_to :phone
-         has_one :address
-         accepts_nested_attributes_for :address
+  # 予約したイベント、現在出展中のイベント、すでに予約済のイベントの条件付きアソシエーション
+  has_many :buyed_events, foreign_key: "buyer_id", class_name: "Event"
+  has_many :exhibiting_events, -> { where("buyer_id is NULL") }, foreign_key: "exhibitor_id", class_name: "Event"
+  has_many :reserved_events, -> { where("buyer_id is not NULL") }, foreign_key: "exhibitor_id", class_name: "Event"
+  
+  has_many :messages
+  has_many :events
+  has_many :comments
+  has_many :cards
+  has_one  :personal
+  accepts_nested_attributes_for :personal
+  belongs_to :phone
+  has_one :address
+  accepts_nested_attributes_for :address
 end
