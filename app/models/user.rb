30e3fc0651b61195c56cec01dfed59b2c8c9968a
rungo728 <rufii72865@gmail.com>
2020-01-31 00:34:11 +0900

    delete　association
    messagesテーブルはイベント予約者グループページで使用する情報を入れるためのもの、グループの定義はせず、buyer_idで入室制限をするのでgroupsとmembersテーブルは不要と判断し、一旦削除するのでアソシエーションも削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_many :groups, through: :members
-         has_many :members
          has_many :messages
          has_many :events
          has_many :comments
