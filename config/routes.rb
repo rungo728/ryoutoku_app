cdede8623e24c4fdeb0ef51efc29f3d0807c068e
rungo728 <rufii72865@gmail.com>
2020-02-04 23:28:42 +0900

    Update routes.rb
    seedで仮情報をイベントテーブルに入れたらこの位置に記述を移すまでは仮置き後で移動

--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :events do
     collection do
-      get 'confirmation', to: 'events#confirmation'    
+      get 'confirmation', to: 'events#confirmation'
+      resources :messages
     end
+    # seedで仮情報をイベントテーブルに入れたらこの位置に記述を移す
+    # resources :messages
   end
 
   resources :exhibits do    
   end
 
-  resources :messages do
-    
-  end
+     
   resources :users do
     
   end
