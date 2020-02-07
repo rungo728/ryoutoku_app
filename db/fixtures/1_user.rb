6ada6c19b20d925d9dfbe6d67878aeb222e31561
rungo728 <rufii72865@gmail.com>
2020-02-07 22:01:06 +0900

    create test data
    サーバーの動きを見るために、イベント関連で入れる情報のテストデータを作成

---
+++ db/fixtures/1_user.rb
@@ -0,0 +1,11 @@
+User.seed(:id,
+  { :id => 1, :nickname => "KA", :email => "test3@gmail.com", :encrypted_password => "111111", :icon => "", :profile => "こんにちは", },
+  { :id => 2, :nickname => "hana", :email => "hanako@gmail.com", :encrypted_password => "222222", :icon => "", :profile => "こんにちは", },
+  { :id => 3, :nickname => "ichiro", :email => "ichiro@gmail.com", :encrypted_password => "333333", :icon => "", :profile => "こんにちは", },
+  { :id => 4, :nickname => "momoko", :email => "momoko@gmail.com", :encrypted_password => "444444", :icon => "", :profile => "こんにちは", },
+  { :id => 5, :nickname => "shinnoshike", :email => "shinn@gmail.com", :encrypted_password => "555555", :icon => "", :profile => "こんにちは", },
+  { :id => 6, :nickname => "tae", :email => "@gmail.com", :encrypted_password => "666666", :icon => "", :profile => "こんにちは", },
+  { :id => 7, :nickname => "ayumu", :email => "ayumu@gmail.com", :encrypted_password => "777777", :icon => "", :profile => "こんにちは", },
+  { :id => 8, :nickname => "akane", :email => "akane@gmail.com", :encrypted_password => "888888", :icon => "", :profile => "こんにちは", },
+  { :id => 9, :nickname => "tetuo", :email => "tetuo@gmail.com", :encrypted_password => "999999", :icon => "", :profile => "こんにちは", }
+)
