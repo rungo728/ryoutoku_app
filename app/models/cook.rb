2df7cc671b05d1c3e4ad36999deb11686d88f035
rungo728 <rufii72865@gmail.com>
2020-02-08 21:39:38 +0900

    fix association

--- app/models/cook.rb
+++ app/models/cook.rb
@@ -1,3 +1,3 @@
 class Cook < ApplicationRecord
-  belongs_to event
+  belongs_to :event
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -5,5 +5,7 @@ class Event < ApplicationRecord
   belongs_to :category
   has_many :messages
   has_many :images
+  has_one :cook
+  has_one :address
   has_many :users, through: :messages
 end
