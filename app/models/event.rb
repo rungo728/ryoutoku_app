df1e9c29259f78dd21eaff552cf58d6725adbfff
rungo728 <rufii72865@gmail.com>
2020-02-15 23:48:49 +0900

    fix association

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,9 +6,11 @@ class Event < ApplicationRecord
   has_many :messages,  dependent: :destroy
   has_many :entries, dependent: :destroy
   has_many :images,dependent: :destroy
-  accepts_nested_attributes_for :images, allow_destroy: true
-  #イメージをDB登録するために記載
   has_one :cook
   has_one :address
+  #イメージ・開催場所日時・料理工程選択をDB登録するために記載
+  accepts_nested_attributes_for :images, allow_destroy: true
+  accepts_nested_attributes_for :cook
+  accepts_nested_attributes_for :address
   has_many :users, through: :messages
 end
