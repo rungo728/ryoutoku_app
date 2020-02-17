478f614c1aebb4de3ecfae9499ba26f66f413171
rungo728 <rufii72865@gmail.com>
2020-02-17 23:28:33 +0900

    add images validate

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,9 +8,14 @@ class Event < ApplicationRecord
   has_many :images,dependent: :destroy
   has_one :cook
   has_one :address
+  has_many :users, through: :messages
+
   #イメージ・開催場所日時・料理工程選択をDB登録するために記載
-  accepts_nested_attributes_for :images, allow_destroy: true
+  accepts_nested_attributes_for :images,allow_destroy: true
   accepts_nested_attributes_for :cook
   accepts_nested_attributes_for :address
-  has_many :users, through: :messages
+  
+  validates_associated :images
+  validates :images, presence: true
+
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,4 @@
 class Image < ApplicationRecord
-  belongs_to :event
-  mount_uploader :image, ImageUploader
+  belongs_to :event,optional: true
+  # mount_uploader :content, ImageUploader
 end
