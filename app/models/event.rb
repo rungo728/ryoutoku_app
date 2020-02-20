012b47fe2be29914e4590fa62de0f8fe6ee2923c
rungo728 <rufii72865@gmail.com>
2020-02-21 05:44:09 +0900

    Update event.rb
    buyer_id（予約者）は出展時はnullでOKなので、記述

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :exhibitor, class_name: "User"
-  belongs_to :buyer, class_name: "User"
+  belongs_to :buyer, class_name: "User",optional: true
   belongs_to :prefecture
   belongs_to :category
   has_many :messages,  dependent: :destroy
