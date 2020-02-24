0140a7f766cff97314cec57e9cb02baa968b7f83
rungo728 <rufii72865@gmail.com>
2020-02-24 23:25:05 +0900

    create card table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_08_161934) do
+ActiveRecord::Schema.define(version: 2020_02_23_170115) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "event_id", null: false
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 2020_02_08_161934) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["event_id"], name: "index_addresses_on_event_id"
+  end
+
+  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "customer_id", null: false
+    t.string "card_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_cards_on_user_id"
   end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -113,6 +122,7 @@ ActiveRecord::Schema.define(version: 2020_02_08_161934) do
   end
 
   add_foreign_key "addresses", "events"
+  add_foreign_key "cards", "users"
   add_foreign_key "cooks", "events"
   add_foreign_key "entries", "events"
   add_foreign_key "entries", "users"
