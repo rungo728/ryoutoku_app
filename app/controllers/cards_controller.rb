8fa3f0388b4109a60a53474034a281282e1734a7
rungo728 <rufii72865@gmail.com>
2020-02-25 23:53:02 +0900

    set show action controller

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,9 +1,12 @@
 class CardsController < ApplicationController
   
   require "payjp" 
+  before_action :set_card
 
   def new
-    card = Card.where(user_id: current_user.id)
+    # cardがすでに登録済みの場合、showのページに戻します。
+    @card = Card.where(user_id: current_user.id).first
+    redirect_to action: "show" if @card.present?    
   end
 
   def pay
@@ -36,6 +39,45 @@ class CardsController < ApplicationController
   end
 
   def show
-    
+
+    # すでにクレジットカードが登録しているか？  
+    if @card.present?
+      # 登録している場合,PAY.JPからカード情報を取得する
+      # PAY.JPの秘密鍵をセットする。
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      # PAY.JPから顧客情報を取得する。
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      # PAY.JPの顧客情報から、デフォルトで使うクレジットカードを取得する。
+      @card_info = customer.cards.retrieve(@card.card_id)
+      # クレジットカード情報から表示させたい情報を定義する。
+      # クレジットカードの画像を表示するために、カード会社を取得
+      @card_brand = @card_info.brand
+      # クレジットカードの有効期限を取得
+      @exp_month = @card_info.exp_month.to_s
+      @exp_year = @card_info.exp_year.to_s.slice(2,3)
+      # クレジットカード会社を取得したので、カード会社の画像をviewに表示させるため、ファイルを指定する。
+      case @card_brand
+      when "Visa"
+        @card_image = "visa.svg"
+      when "JCB"
+        @card_image = "jcb.svg"
+      when "MasterCard"
+        @card_image = "master-card.svg"
+      when "American Express"
+        @card_image = "american_express.svg"
+      when "Diners Club"
+        @card_image = "dinersclub.svg"
+      when "Discover"
+        @card_image = "discover.svg"
+      end
+    end
+  end
+
+  private
+
+  def set_card
+    @card = Card.where(user_id: current_user.id).first if Card.where(user_id: current_user.id).present?
   end
 end
+
+
