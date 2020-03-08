bb5fe2928194080220668eae4c6ecc58150101e1
rungo728 <rufii72865@gmail.com>
2020-03-08 17:39:48 +0900

    update　deploy2

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,6 +20,14 @@ set :keep_releases, 5
 
 # secrets.yml用のシンボリックリンクを追加
 set :linked_files, %w{ config/secrets.yml }
+
+# 環境変数をcapistranoでの自動デプロイで使用
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
 # 元々記述されていた after 「'deploy:publishing', 'deploy:restart'」以下を削除して、次のように書き換え
 # after 'deploy:publishing', 'deploy:restart'
 # namespace :deploy do
