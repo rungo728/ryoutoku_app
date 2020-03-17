122af143125852c68f9476cdc5f69cb4048abb16
rungo728 <rufii72865@gmail.com>
2020-03-17 22:44:13 +0900

    Update events_controller.rb
    未ログイン時はトップページとイベント詳細ページのみ見れるように修正

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!,except: [:index,:show]
   before_action :get_user
   before_action :set_event,only: [:confirmation, :done,:edit, :update]
 
